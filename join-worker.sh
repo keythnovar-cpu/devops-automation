95b027ef90891e8e1399e92525306f43cc6c76b1
keythnovar-cpu <keythnovar@gmail.com>
2026-03-02 14:01:27 +0530

    Add shebang and set error handling in join-worker.sh

--- join-worker.sh
+++ join-worker.sh
@@ -1,3 +1,4 @@
-sudo kubeadm join 192.168.56.101:6443 \
-  --token g12rm7.me28unav4sh8xsk \
-  --discovery-token-ca-cert-hash sha256:f6133841f8cc80c4f18e2defbebb066e175307a1fb14c43846c8a3a1246ad161
+#!/bin/bash
+set -e
+
+sudo kubeadm join 192.168.56.101:6443 --token g12rm7.me28unav4sh8xsk --discovery-token-ca-cert-hash sha256:f6133841f8cc80c4f18e2defbebb066e175307a1fb14c43846c8a3a1246ad161
